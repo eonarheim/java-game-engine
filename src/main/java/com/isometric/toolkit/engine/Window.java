26e26f039a9c82f889ad8320d5e87f162612d217
Erik Onarheim <onarh001@umn.edu>
2012-06-03 23:28:20 -0500

    More updates

--- src/main/java/com/isometric/toolkit/engine/Window.java
+++ src/main/java/com/isometric/toolkit/engine/Window.java
@@ -68,34 +68,13 @@ public class Window
     // TODO: Implement FPS loading limiting
     while (!Display.isCloseRequested() && !Keyboard.isKeyDown(Keyboard.KEY_ESCAPE)) {
 
-      
-      
       gameWorld.update();
       gameWorld.draw();
       
-      
-      // TODO: Render code here
-      GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
-      GL11.glMatrixMode(GL11.GL_MODELVIEW);
-      GL11.glLoadIdentity();
-      
-      // Clear the screen and depth buffer
-          
-      // set the color of the quad (R,G,B,A)
-      GL11.glColor3f(0.5f,0.5f,1.0f);
-          
-      // draw quad
-      GL11.glBegin(GL11.GL_QUADS);
-          GL11.glVertex2f(100,100);
-          GL11.glVertex2f(100+200,100);
-          GL11.glVertex2f(100+200,100+200);
-          GL11.glVertex2f(100,100+200);
-      GL11.glEnd();
-      
-
-      checkInput();
+      //checkInput();
       Display.update();
       Display.sync(60);
+      
       
       
     }
--- src/main/java/com/isometric/toolkit/engine/World.java
+++ src/main/java/com/isometric/toolkit/engine/World.java
@@ -4,6 +4,7 @@ import java.io.InputStream;
 import java.util.HashMap;
 
 import org.apache.log4j.Logger;
+import org.lwjgl.opengl.GL11;
 
 import com.isometric.toolkit.entities.Actor;
 import com.isometric.toolkit.entities.Level;
@@ -17,7 +18,11 @@ public class World
   
   private HashMap<String,Level> levels = new HashMap<String, Level>();
   
+  private String currentLevel = "";
+  
   private TextureLoader texture = null;
+  
+  private String worldName = "";
   
   
   public World(InputStream in){
@@ -30,6 +35,7 @@ public class World
     // TODO: Call parser to create hashmap data structures from world file
     
     
+    
   }
   
   public void update(){
@@ -38,6 +44,62 @@ public class World
   
   public void draw(){
     
+    // TODO: Render code here
+    GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+    GL11.glMatrixMode(GL11.GL_MODELVIEW);
+    GL11.glLoadIdentity();
+    
+    // Clear the screen and depth buffer
+        
+    // set the color of the quad (R,G,B,A)
+    GL11.glColor3f(0.5f,0.5f,1.0f);
+        
+    // draw quad
+    GL11.glBegin(GL11.GL_QUADS);
+        GL11.glVertex2f(100,100);
+        GL11.glVertex2f(100+200,100);
+        GL11.glVertex2f(100+200,100+200);
+        GL11.glVertex2f(100,100+200);
+    GL11.glEnd();
+    
+  }
+  
+  public void addActor(String key, Actor a){
+    actors.put(key, a);
+  }
+  
+  public void addLevel(String key, Level l){
+    levels.put(key, l);
+  }
+  
+  public void removeActor(String key){
+    actors.remove(key);
+  }
+  
+  public void removeLevel(String key){
+    levels.remove(key);
+  }
+  
+  
+
+  public String getWorldName ()
+  {
+    return worldName;
+  }
+
+  public void setWorldName (String worldName)
+  {
+    this.worldName = worldName;
+  }
+
+  public String getCurrentLevel ()
+  {
+    return currentLevel;
+  }
+
+  public void setCurrentLevel (String currentLevel)
+  {
+    this.currentLevel = currentLevel;
   }
 
 }
