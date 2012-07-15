794f2a415638fbb16e0cff7307b13a07f361dbc1
jcbrock <betapi1739@gmail.com>
2012-07-15 14:28:59 -0500

    Added code to read in multiple row sprite sheets

--- src/main/java/com/isometric/toolkit/engine/SpriteSheet.java
+++ src/main/java/com/isometric/toolkit/engine/SpriteSheet.java
@@ -8,27 +8,33 @@ public class SpriteSheet
   private Image internalImage;
   private String internalImagePath = "";
   private List<Image> images = new ArrayList<Image>();
-  
-  
-  public SpriteSheet(String ref, int horizontalCount, int verticalCount){
+
+  public SpriteSheet (String ref, int horizontalCount, int verticalCount)
+  {
     setInternalImagePath(ref);
     internalImage = Image.loadImage(ref);
     int w = internalImage.getWidth();
-    int spacing = (int) Math.floor(w/horizontalCount);
-    
-    for(int i = 0; i < horizontalCount; i++){
-      images.add(Image.loadSubimage(ref, spacing, internalImage.getHeight(), i));
+    int h = internalImage.getHeight();
+    int verticalSpacing = (int) Math.floor(w / horizontalCount);
+    int horizontalSpacing = (int) Math.floor(h / verticalCount);
+
+    for (int j = 0; j < verticalCount; j++) {
+      for (int i = 0; i < horizontalCount; i++) {
+        images.add(Image.loadSubimage(ref, verticalSpacing, horizontalSpacing,
+                                      i, j));
+      }
     }
-    
+
   }
-  
-  public List<Image> getImages(int from, int to){
+
+  public List<Image> getImages (int from, int to)
+  {
     List<Image> resultImages = new ArrayList<Image>();
-    for(int i= from; i <= to; i++){
+    for (int i = from; i <= to; i++) {
       resultImages.add(images.get(i));
     }
     return resultImages;
-    
+
   }
 
   public String getInternalImagePath ()
