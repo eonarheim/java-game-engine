8c5927174a71a4efce31ddd0f2743bf70d85da2f
betapi1739@gmail.com <betapi1739@gmail.com>
2012-08-07 20:12:48 -0500

    Editor is still very prototypy, but at least have a panel with a grid
    The grid can detect the mouse's x,y click coordinates as well.

--- src/main/java/com/isometric/toolkit/editor/Editor.java
+++ src/main/java/com/isometric/toolkit/editor/Editor.java
@@ -1,6 +1,8 @@
 package com.isometric.toolkit.editor;
 
 import java.awt.*;
+
+import javax.swing.BorderFactory;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
@@ -8,6 +10,10 @@ import java.awt.event.MouseEvent;
 import java.lang.reflect.Method;
 import com.sun.awt.*;
 import javax.swing.*;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
 
 //Following this naming covention:
 //JFC (Java Swing) variables should be suffixed by the element type.
@@ -15,13 +21,16 @@ import javax.swing.*;
 public class Editor {
 	public void display() {
 		final JFrame f = new JFrame("Swing Editor");
-		f.setSize(300, 300);
+		f.setSize(500, 500);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		f.setLayout(new FlowLayout());
+		//f.setLayout(new FlowLayout());
+		 f.setLayout(new BoxLayout(f.getContentPane(), BoxLayout.Y_AXIS));
 		f.setVisible(true);
 		JLabel jl = new JLabel("Level Editor");
 		f.add(jl);
-
+		
+		//f.setPreferredSize(new Dimension(5,5));
+		
 		// Create Exit button
 		JButton exitButton = new JButton("Exit");
 		// Add event handler for button
@@ -33,8 +42,9 @@ public class Editor {
 		// Add button to a panel
 		JPanel buttonPanel = new JPanel();
 		buttonPanel.add(exitButton);
-		f.getContentPane().add(buttonPanel, BorderLayout.SOUTH);
-
+		//f.getContentPane().add(buttonPanel, BorderLayout.SOUTH);
+		f.add(buttonPanel);
+		
 		String[] objectList = { "Grass", "PlayerDude" };
 		final JComboBox objectComboBox = new JComboBox(objectList);
 		objectComboBox.addActionListener(new ActionListener() {
@@ -43,20 +53,52 @@ public class Editor {
 						+ objectComboBox.getSelectedItem().toString());
 			}
 		});
-		Container cont = f.getContentPane();
-		cont.setLayout(new FlowLayout());
-		cont.add(objectComboBox);
+		//ComboBox
+		//Container cont = f.getContentPane();
+		//cont.setLayout(new FlowLayout());
+		//cont.add(objectComboBox);
+		f.add(objectComboBox);
 
-		JPanel myPanel = new JPanel();
-		myPanel.setSize(150, 150);
-		myPanel.setOpaque(true);
-		myPanel.setDoubleBuffered(false);
-		myPanel.setBackground(new Color(30, 190, 250));
+//		JPanel myPanel = new JPanel(new GridLayout(0, 2));
+//		myPanel.add(new JLabel("JLabel 1"));
+//		myPanel.add(new JButton("JButton 2"));
+//		myPanel.add(new JCheckBox("JCheckBox 3"));
+//		myPanel.add(new JTextField("Long-Named JTextField 4"));
+//		myPanel.add(new JButton("JButton 5"));
+//
+//		//myPanel.setSize(300, 150);
+//		myPanel.setOpaque(true);
+//		myPanel.setDoubleBuffered(false);
+//		myPanel.setBackground(new Color(30, 190, 250));
+//		// myPanel.setForeground(Color.CYAN);
+//		// myPanel.setLayout(new BorderLayout()); //This is what f's it up. I
+//		// think it wraps so close around btn
+//
+//		f.add(myPanel);
+		
+		
+		//Wtf, how do I force a minimum number of grids
+		//Border border = new BorderFactory.createEmptyBorder();// EmptyBorder(1, 1, 1, 1);
+		JPanel myPanel2 = new JPanel(new GridLayout(5, 8));
+		//myPanel2.setBorder(new EmptyBorder(1, 1, 1, 1));
+		for (int i = 0; i < 40; i++)
+		{
+			myPanel2.add(new JLabel());
+		}
+
+		myPanel2.setSize(50, 50); //currently doesn't seem to do anything
+		myPanel2.setOpaque(true);
+		myPanel2.setDoubleBuffered(false);
+		myPanel2.setBackground(new Color(0, 0, 0));
 		// myPanel.setForeground(Color.CYAN);
 		// myPanel.setLayout(new BorderLayout()); //This is what f's it up. I
 		// think it wraps so close around btn
-
-		f.getContentPane().add(myPanel, BorderLayout.SOUTH);
+		for (Component j: myPanel2.getComponents())
+		{
+			((JLabel)j).setBorder(new LineBorder(Color.YELLOW));
+		}
+		
+		f.add(myPanel2);
 
 		// JButton btn1 = new JButton("Add, but no validation");
 		// JButton btn2 = new JButton("Add and validate");
@@ -65,7 +107,7 @@ public class Editor {
 
 		// JFrame f = new JFrame();
 		// f.getContentPane().add(visiblePanel);
-		f.getContentPane().add(myPanel, BorderLayout.SOUTH);
+		// f.getContentPane().add(myPanel, BorderLayout.SOUTH);
 
 		// GraphicsEnvironment env =
 		// GraphicsEnvironment.getLocalGraphicsEnvironment();
@@ -115,31 +157,35 @@ public class Editor {
 		myBut.setContentAreaFilled(true);
 		myBut.setSize(100, 50);
 		myBut.setLocation(50, 25);
-		myPanel.add(myBut);
+		//myPanel.add(myBut);
 
 		myBut.addMouseListener(new MouseAdapter() {
 			public void mouseClicked(MouseEvent e) {
-				System.out.println("you clicked my button");
+				System.out.println("you clicked my button ~ x:" + e.getX()
+						+ " y: " + e.getY());
 			}
 		});
 
-		myPanel.addMouseListener(new MouseAdapter() {
+		myPanel2.addMouseListener(new MouseAdapter() {
 			public void mouseClicked(MouseEvent e) {
-				System.out.println("you clicked my panel");
+				System.out.println("you clicked my panel ~ x:" + e.getX()
+						+ " y: " + e.getY());
 			}
 		});
 
 		f.addMouseListener(new MouseAdapter() {
 			public void mouseClicked(MouseEvent e) {
-				System.out.println("you clicked my window");
+				System.out.println("you clicked my window ~ x:" + e.getX()
+						+ " y: " + e.getY());
 			}
 		});
 
 	}
 
-	//Test code:
+	// Test code:
 	JPanel visiblePanel = new JPanel(new FlowLayout()); // is the default, but
 														// showing it set
+
 	public void buildGUI() {
 		visiblePanel.setBorder(BorderFactory
 				.createTitledBorder("Visible Panel"));
