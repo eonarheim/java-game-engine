2004b1ad52b022c5d6b825080a0c346b90aaf40b
betapi1739@gmail.com <betapi1739@gmail.com>
2012-08-10 16:46:35 -0500

    #27 Loading all the files within the "images/" folder
    This might not be the best solution, but it is a start for now.

--- src/main/java/com/isometric/toolkit/editor/Editor.java
+++ src/main/java/com/isometric/toolkit/editor/Editor.java
@@ -13,8 +13,10 @@ import java.awt.event.MouseEvent;
 import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Method;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.util.Enumeration;
 
 import com.sun.awt.*;
 import javax.swing.*;
@@ -26,8 +28,8 @@ import javax.swing.border.LineBorder;
 import org.lwjgl.LWJGLException;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
-import java.awt.Graphics; 
-import java.awt.image.BufferedImage; 
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
 
 import com.isometric.toolkit.engine.Animation;
 import com.isometric.toolkit.engine.Image;
@@ -37,16 +39,17 @@ import com.isometric.toolkit.engine.Image;
 //http://geosoft.no/development/javastyle.html
 public class Editor {
 	private Icon imageIcon;
+
 	public void display() {
 		final JFrame f = new JFrame("Swing Editor");
 		f.setSize(500, 500);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		//f.setLayout(new FlowLayout());
-		 f.setLayout(new BoxLayout(f.getContentPane(), BoxLayout.Y_AXIS));
+		// f.setLayout(new FlowLayout());
+		f.setLayout(new BoxLayout(f.getContentPane(), BoxLayout.Y_AXIS));
 		f.setVisible(true);
 		JLabel jl = new JLabel("Level Editor");
 		f.add(jl);
-		
+
 		// Create Exit button
 		JButton exitButton = new JButton("Exit");
 		// Add event handler for button
@@ -58,11 +61,26 @@ public class Editor {
 		// Add button to a panel
 		JPanel buttonPanel = new JPanel();
 		buttonPanel.add(exitButton);
-		//f.getContentPane().add(buttonPanel, BorderLayout.SOUTH);
+		// f.getContentPane().add(buttonPanel, BorderLayout.SOUTH);
 		f.add(buttonPanel);
-		
-		//Create/Add combo box
-		String[] objectList = { "Grass", "PlayerDude" }; //TODO - Don't hard code
+
+		// Create/Add combo box - load files from "images/" folder
+		String[] objectList = null;
+		File dir = null;
+		try {
+			dir = new File(ClassLoader.getSystemResource("images/").toURI());
+		} catch (URISyntaxException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+		// Set combo box list - do some checking on it
+		objectList = dir.list();
+		if (objectList == null)
+			System.out.println("No resource files found"); // TODO Change these to log file
+		else if (objectList.length == 0)
+			System.out.println("No resource files found");
+
 		final JComboBox objectComboBox = new JComboBox(objectList);
 		objectComboBox.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -71,31 +89,32 @@ public class Editor {
 			}
 		});
 		f.add(objectComboBox);
-	
+
 		final int gridRowCount = 5;
 		final int gridColCount = 8;
-		//Wtf, how do I force a minimum number of grids - I need to create a label for each one
-		
-		final JPanel myPanel = new JPanel(new GridLayout(gridRowCount, gridColCount)) ;		
-		for (int i = 0; i < (gridRowCount * gridColCount); i++)
-		{
+		// Wtf, how do I force a minimum number of grids - I need to create a
+		// label for each one
+
+		final JPanel myPanel = new JPanel(new GridLayout(gridRowCount,
+				gridColCount));
+		for (int i = 0; i < (gridRowCount * gridColCount); i++) {
 			JLabel j = new JLabel();
 			j.setBorder(new LineBorder(Color.YELLOW));
 			j.setOpaque(true);
 			j.setBackground(Color.BLACK);
 			myPanel.add(j);
 		}
-		myPanel.setSize(50, 50); //currently doesn't seem to do anything
+		myPanel.setSize(50, 50); // currently doesn't seem to do anything
 		myPanel.setOpaque(true);
 		myPanel.setDoubleBuffered(false);
 		myPanel.setBackground(new Color(0, 0, 0));
-		
-//		for (Component j: myPanel2.getComponents())
-//		{
-//			((JLabel)j).setBorder(new LineBorder(Color.YELLOW));
-//			((JLabel)j).setOpaque(true);
-//			((JLabel)j).setBackground(Color.BLACK);
-//		}		
+
+		// for (Component j: myPanel2.getComponents())
+		// {
+		// ((JLabel)j).setBorder(new LineBorder(Color.YELLOW));
+		// ((JLabel)j).setOpaque(true);
+		// ((JLabel)j).setBackground(Color.BLACK);
+		// }
 		f.add(myPanel);
 
 		final JButton myBut = new JButton();
@@ -105,15 +124,16 @@ public class Editor {
 		myBut.setContentAreaFilled(true);
 		myBut.setSize(100, 50);
 		myBut.setLocation(50, 25);
-		
-		  try {                 
-			  BufferedImage image = ImageIO.read(Animation.class.getClassLoader()
-  	                .getSystemResourceAsStream("images/TestPlayer.png"));
-			  imageIcon = new ImageIcon(image);
-	      } catch (IOException ex) { 
-	            //TODO handle exception... 
-	       }     
-		
+
+		try {
+			BufferedImage image = ImageIO.read(Animation.class.getClassLoader()
+					.getSystemResourceAsStream("images/TestPlayer.png"));
+			imageIcon = new ImageIcon(image);
+		} catch (IOException ex) {
+			// TODO handle exception...
+			ex.printStackTrace();
+		}
+
 		myBut.addMouseListener(new MouseAdapter() {
 			public void mouseClicked(MouseEvent e) {
 				System.out.println("you clicked my button ~ x:" + e.getX()
@@ -125,23 +145,31 @@ public class Editor {
 			public void mouseClicked(MouseEvent e) {
 				System.out.println("you clicked my panel ~ x:" + e.getX()
 						+ " y: " + e.getY());
-				System.out.println("Panel Width: " + myPanel.getWidth() +
-						" Panel Height: " + myPanel.getHeight());
+				System.out.println("Panel Width: " + myPanel.getWidth()
+						+ " Panel Height: " + myPanel.getHeight());
 				double rowHeight = myPanel.getHeight() / gridRowCount;
 				double colWidth = myPanel.getWidth() / gridColCount;
 				double rowClicked = Math.floor((e.getY() / rowHeight));
-				double colClicked = Math.floor((e.getX() / colWidth));//why do i have to make rowcounts final? 
-				System.out.println("Cell: " + rowClicked + "," + colClicked);						
-				JLabel clickedLabel = (JLabel)myPanel.getComponent((int) ((rowClicked * gridColCount) + colClicked));
-				
+				double colClicked = Math.floor((e.getX() / colWidth));// why do
+																		// i
+																		// have
+																		// to
+																		// make
+																		// rowcounts
+																		// final?
+				System.out.println("Cell: " + rowClicked + "," + colClicked);
+				JLabel clickedLabel = (JLabel) myPanel
+						.getComponent((int) ((rowClicked * gridColCount) + colClicked));
+
 				if (clickedLabel.getIcon() != imageIcon)
-					clickedLabel.setIcon(imageIcon); //if imageIcon is null, it just shows nothing				
+					clickedLabel.setIcon(imageIcon); // if imageIcon is null, it
+														// just shows nothing
 				else
 					clickedLabel.setIcon(null);
-					//clickedLabel.setBackground(Color.BLACK);			
-				//clickedLabel.setOpaque(true);
-				//clickedLabel.setForeground(Color.GREEN);
-				//clickedLabel.setText("c");
+				// clickedLabel.setBackground(Color.BLACK);
+				// clickedLabel.setOpaque(true);
+				// clickedLabel.setForeground(Color.GREEN);
+				// clickedLabel.setText("c");
 			}
 		});
 
@@ -156,6 +184,7 @@ public class Editor {
 	// Test code:
 	JPanel visiblePanel = new JPanel(new FlowLayout()); // is the default, but
 														// showing it set
+
 	public void buildGUI() {
 		visiblePanel.setBorder(BorderFactory
 				.createTitledBorder("Visible Panel"));
