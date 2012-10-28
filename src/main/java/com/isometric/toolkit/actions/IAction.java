d51589865ae67c26b8e9a19f2e96b04f611c76d7
Neutronite <onarh001@umn.edu>
2012-10-28 14:08:26 -0500

    Forgot to add IAction

---
+++ src/main/java/com/isometric/toolkit/actions/IAction.java
@@ -0,0 +1,44 @@
+package com.isometric.toolkit.actions;
+
+import com.isometric.toolkit.engine.Point;
+import com.isometric.toolkit.entities.Actor;
+import com.isometric.toolkit.exceptions.RepeatForeverException;
+
+/***
+ * Base action from which all other derive.
+ * 
+ * @author Erik
+ *
+ */
+public interface IAction
+{
+    
+  
+  /***
+   * Returns the current position of the actor
+   * @return Point
+   */
+  public Point getPos();
+  
+    
+  /***
+   * Updates the actor in a certain way for the scripted action
+   */
+  public void update(float delta);
+  
+  
+  /***
+   * Indicates whether an action is complete
+   */
+  public boolean isComplete(Actor a);
+
+
+  /***
+   * Indicates whether an action has started execution
+   * @return boolean
+   */
+  public boolean hasStarted();
+    
+ 
+
+}
